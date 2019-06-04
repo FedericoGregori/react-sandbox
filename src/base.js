a705c659046a42a4892dad4503a09566c001d3d2
Federico Gregori <jfedericogregori@gmail.com>
2019-06-04 12:06:06 -0300

    [UPD] Login config

--- src/base.js
+++ src/base.js
@@ -2,9 +2,9 @@ import Rebase from "re-base";
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyDXUCNSpi5u07F76httlCTXAA4mPVQlEHs",
-  authDomain: "catch-of-the-day-wes-bos-2.firebaseapp.com",
-  databaseURL: "https://catch-of-the-day-wes-bos-2.firebaseio.com"
+    apiKey: "AIzaSyBlH5wK1Q81_8OPzZqnQqqbO9vSc8NmE5U",
+    authDomain: "catch-of-the-day-fede.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-fede.firebaseio.com"
 });
 
 const base = Rebase.createClass(firebaseApp.database());
